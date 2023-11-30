4c06fe93c808a514a08272367bcd3ef50be28006
Jack <jack.x.chen@outlook.com>
2023-11-30 21:20:12 +1000

    first commit

--- assignment-2/knexfile.js
+++ assignment-2/knexfile.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 module.exports = {
     client: 'mysql2',
     connection: {
-        host: '127.0.0.1',
+        host: process.env.DB_HOST,
         database: 'movies',
-        user: 'root',
-        password: 'Y8acl9in9985zkl61@'
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS
     }
 }
